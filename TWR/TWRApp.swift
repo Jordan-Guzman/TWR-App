6d2b02b8f684978132158d90a436d0dd7a206bc6
Jordan-Guzman <jordanhguz@gmail.com>
2022-10-23 14:03:00 -0700

    XCode project created

---
+++ TWR/TWRApp.swift
@@ -0,0 +1,17 @@
+//
+//  TWRApp.swift
+//  TWR
+//
+//  Created by Jordan Guzman on 10/23/22.
+//
+
+import SwiftUI
+
+@main
+struct TWRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
