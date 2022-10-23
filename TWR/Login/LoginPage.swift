832d0210175fef552431a0da129139b0fc3a4383
Jordan-Guzman <jordanhguz@gmail.com>
2022-10-23 16:56:18 -0700

    Changed the login screen color to be a darker blue

--- TWR/Login/LoginPage.swift
+++ TWR/Login/LoginPage.swift
@@ -8,9 +8,10 @@
 import SwiftUI
 
 struct LoginPage: View {
+    @State private var username: String = ""
     var body: some View {
         ZStack {
-            Color.blue.ignoresSafeArea()
+            Color("TWR Blue").ignoresSafeArea()
             Image("Logo").position(x:UIScreen.main.bounds.width/2,
                 y:UIScreen.main.bounds.height/5
              )
