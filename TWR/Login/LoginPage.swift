2b0a2b9fe0c6f46cdbad5cab0264bcad6029cec1
Jordan-Guzman <jordanhguz@gmail.com>
2022-10-23 15:25:47 -0700

    Added Login folder, LoginPage file, and the Tuesdays Waterfront logo to the login page

---
+++ TWR/Login/LoginPage.swift
@@ -0,0 +1,23 @@
+//
+//  LoginPage.swift
+//  TWR
+//
+//  Created by Jordan Guzman on 10/23/22.
+//
+
+import SwiftUI
+
+struct LoginPage: View {
+    var body: some View {
+        ZStack {
+            Color.blue.ignoresSafeArea()
+            Image("Logo")
+        }
+    }
+}
+
+struct LoginPage_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginPage()
+    }
+}
