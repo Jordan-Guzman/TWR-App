2510b058a85bdf30a779fa2588ee3c0b365728a6
Jordan-Guzman <jordanhguz@gmail.com>
2022-10-28 07:10:13 -0700

    Added login and register buttons. Not functionality to the buttons yet. Still need to make the actual login and register forms.

--- TWR/Login/LoginPage.swift
+++ TWR/Login/LoginPage.swift
@@ -13,8 +13,42 @@ struct LoginPage: View {
         ZStack {
             Color("TWR Blue").ignoresSafeArea()
             Image("Logo").position(x:UIScreen.main.bounds.width/2,
-                y:UIScreen.main.bounds.height/5
+                y:UIScreen.main.bounds.height/4
              )
+            Button(action: {
+                print("Delete tapped!")
+            }) {
+                HStack {
+                    Text("Login")
+                        .fontWeight(.semibold)
+                        .font(.title)
+                        .foregroundColor(Color("TWR Blue"))
+                }
+                .frame(minWidth: 0, maxWidth: UIScreen.main.bounds.width/2)
+                .padding()
+                .foregroundColor(.white)
+                .background(Color.white)
+//                .background(LinearGradient(gradient: Gradient(colors: [Color.green, Color("LightGreen")]), startPoint: .leading, endPoint: .trailing))
+                .cornerRadius(40)
+            }.position(x:UIScreen.main.bounds.width/2,
+                       y:UIScreen.main.bounds.height/1.8)
+            Button(action: {
+                print("Delete tapped!")
+            }) {
+                HStack {
+                    Text("Register")
+                        .fontWeight(.semibold)
+                        .font(.title)
+                        .foregroundColor(Color("TWR Blue"))
+                }
+                .frame(minWidth: 0, maxWidth: UIScreen.main.bounds.width/2)
+                .padding()
+                .foregroundColor(.white)
+                .background(Color.white)
+//                .background(LinearGradient(gradient: Gradient(colors: [Color.green, Color("LightGreen")]), startPoint: .leading, endPoint: .trailing))
+                .cornerRadius(40)
+            }.position(x:UIScreen.main.bounds.width/2,
+                       y:UIScreen.main.bounds.height/1.55)
         }
     }
 }
